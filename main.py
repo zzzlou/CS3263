70e9b12e4cef2d948b4ae96a85b8949ef4c32e49
Zhanzhi Lou <zhanzhilou@gmail.com>
2025-03-31 10:13:09 +0800

    Fix parse error

--- main.py
+++ main.py
@@ -54,12 +54,9 @@ def parse_course_info(code, exam_date, exam_duaration, timetable):
         if lesson_type == "Lecture":
             lecture = Lecture(id=class_no, start_time=start_time,end_time=end_time, day = day, weeks = weeks, venue = venue)
             lectures.append(lecture)
-        elif lesson_type == "Tutorial":
+        else:
             tutorial = Tutorial(id=class_no, start_time=start_time,end_time=end_time, day = day, weeks = weeks, venue = venue)
             tutorials.append(tutorial)
-        else:
-            print("Error, unknown lesson type")
-            return None
     course = Course(code, exam_date, exam_duaration, lectures, tutorials)
     return course
     
